bec4efaf56fda621d397162509c00d5fc18551e6
antinka <antinka.katrinka@gmail.com>
2017-12-26 23:47:14 +0200

    Add project

---
+++ BlogUsingEF/Controllers/HomeController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using BlogUsingEF.DAL.Repositories;
+using BlogUsingEF.DAL;
+using  BlogUsingEF.DAL.Entities;
+using BlogUsingEF.DAL.Interfaces;
+namespace BlogUsingEF.Controllers
+{
+    public class HomeController : Controller
+    {
+        // make variable for working with db
+        IRepository<Article> dbArticle=new ArticleRepository();
+        IRepository<Comment> dbComment = new CommentRepository();
+        IRepository<User> dbUser= new UserRepository(); 
+
+        public ActionResult Index()
+        {
+            //return all article
+            return View( dbArticle.GetList());
+        }
+        public ActionResult ViewMyArrticle()
+        {
+            //return all article which own login user and present for him delete it
+            return View( dbArticle.GetList().Where(u => u.UserId == Int32.Parse((string)(Session["UserId"]))));
+        }
+        [HttpGet]
+        public ActionResult AddArticle()
+        {
+            return View();
+        }
+        //add article where userId take from session data
+        [HttpPost]
+        public ActionResult AddArticle(Article article)
+        {
+            Article art = new Article();
+            art.Text = article.Text;
+            art.Title = article.Title;
+            art.DataPublish = DateTime.Now;
+            string idUser = ((string)(Session["UserId"]));
+            art.UserId = Int32.Parse(idUser);
+            dbArticle.Create(art);
+            dbArticle.Save();
+            return RedirectToAction("Index");
+        }
+        // delete the article and all comment which it has
+        public ActionResult DeleteArticle(int id)
+        {
+           Article article = dbArticle.GetById(id);
+            if(article!=null)
+            {   dbArticle.Delete(id);
+                var IdDeleteComment = (from c in dbComment.GetList() where c.ArticleId == id select c.Id).ToList();
+                for (int i = 0; i < IdDeleteComment.Count; i++)
+                {
+                    dbComment.Delete(IdDeleteComment[i]);
+                }
+                dbComment.Save();
+                dbArticle.Save();
+                
+            }
+            return RedirectToAction("Index");
+        }
+     //return all comment which has the article 
+        public ActionResult ViewComment(int id)
+        {
+            return View(dbComment.GetList().Where(u => u.ArticleId==id));
+        }
+        //add comment to choosen article 
+        [HttpGet]
+        public ActionResult AddComment(int id)
+        {
+            ViewBag.id = id;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddComment(Comment coment,string IdArticle)
+        {
+            coment.DataPublish = DateTime.Now;
+            string idUser = ((string)(Session["UserId"]));
+            coment.UserId = Int32.Parse(idUser);
+            coment.ArticleId = Int32.Parse(IdArticle);
+            dbComment.Create(coment);
+            dbComment.Save();
+            return RedirectToAction("Index");
+
+        }
+
+
+
+    }
+}
---
+++ BlogUsingEF/Controllers/UserController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BlogUsingEF.DAL.Repositories;
+using BlogUsingEF.DAL;
+using BlogUsingEF.DAL.Entities;
+using BlogUsingEF.DAL.Interfaces;
+
+namespace BlogUsingEF.Controllers
+{
+    public class UserController : Controller
+    {
+        IRepository<User> db;
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }  
+        // add new user where userName unic
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registration(User user)
+        {
+            db = new UserRepository();
+            if (ModelState.IsValid)
+            {
+                //check for exist such userName
+                var ifExistUserName = (from b in db.GetList() where b.UserName == user.UserName select b).Count();
+                if (ifExistUserName == 0)
+                {
+                    db.Create(user);
+                    db.Save();
+                    var details = db.GetList().Single(u => u.UserName == user.UserName && u.Password == user.Password);
+                    Session["UserId"] = details.Id.ToString();
+                    Session["UserName"] = details.UserName.ToString();
+                    return RedirectToAction("Welcom");
+                }
+                ModelState.AddModelError("", "this login exist");
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            db = new UserRepository();
+            // search user which try to login and put id and userName in relevant session
+            try
+            {
+                
+                var details = db.GetList().Single(u => u.UserName == user.UserName && u.Password == user.Password);
+                if (details != null)
+                {
+                    Session["UserId"] = details.Id.ToString();
+                    Session["UserName"] = details.UserName.ToString();
+                    return RedirectToAction("Welcom");
+                }
+            }
+            // if such user didnt find
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error");
+            }
+            return View(user);
+
+        }
+        public ActionResult LogOut()
+        {
+            Session.Clear();
+            return RedirectToAction("Welcom");
+        }
+        public ActionResult Welcom()
+        {
+            return View();
+        }
+    }
+}
